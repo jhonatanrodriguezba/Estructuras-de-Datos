0134469e8e953cdae3bd9a3bf34e27313f5944a4
jhonatanrodriguezba <jhonatan.rodriguezba@campusucc.edu.co>
2026-02-11 20:22:34 +0000

    Ejercicio en Clase

---
+++ Sesion2.java
@@ -0,0 +1,14 @@
+public class Sesion2 {
+    public static void main(String[] args) {
+        int[] a = {7, 2, 6, 1, 0, 3};
+
+        for(int i = 0; i < a.length; i++){
+            System.out.println("a[" + i + "]=" + a[i]);
+        }
+        int total = 0;
+        for(int i = 0; i < a.length; i++){
+            total += a[i];
+        }
+        System.out.println("La suma de los elementos del arreglo es " + total);
+    }
+}
